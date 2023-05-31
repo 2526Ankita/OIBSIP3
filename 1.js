18b721211c1e8aa99055c5a22369d22d8a091840
Ankita Choudhary <ankitachoudhary2345@gmail.com>
2023-05-31 13:37:40 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,8 @@
+function show(value) {
+    document.getElementById("result").value+=value;
+  }
+  function calc(){
+    var a=document.getElementById("result").value;
+    var b=eval(a);
+    document.getElementById("result").value=b;
+  }  
